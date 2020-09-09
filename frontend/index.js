c9c2de7d98e5fd2c970f02f8504fd600ccbff38e
anisssoudki <aniss.marrouch@gmail.com>
2020-09-09 10:50:31 -0700

    created canvas using matterjs

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,21 @@
 
-
+// destructuring variables from the library/ see notes line 34
+// im just litteraly using an example giving by the library
+// https://github.com/liabru/matter-js/blob/master/examples/mixed.js
 const { Engine, Render, Runner, World, Bodies} = Matter;
+
+// when we create the engine we get a world object along with it
+ // create engine
+const engine = Engine.create(); 
+const {world} = engine; 
+// create renderer-this is an additive process to document.body in other words it will not destroy any element inside of the body in the html
+const render = Render.create({
+    element: document.body,
+    engine: engine, 
+    option: {
+        width: 800,
+        height: 600
+    }
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
