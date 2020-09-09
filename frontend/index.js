c8bbd30b09456272d5bc9aa6c9cbcae57f0e0e04
anisssoudki <aniss.marrouch@gmail.com>
2020-09-09 11:12:02 -0700

    created test shape

--- frontend/index.js
+++ frontend/index.js
@@ -7,15 +7,23 @@ const { Engine, Render, Runner, World, Bodies} = Matter;
 // when we create the engine we get a world object along with it
  // create engine
 const engine = Engine.create(); 
-const {world} = engine; 
+const { world } = engine; 
 // create renderer-this is an additive process to document.body in other words it will not destroy any element inside of the body in the html
 const render = Render.create({
     element: document.body,
     engine: engine, 
-    option: {
+    options: {
         width: 800,
         height: 600
     }
 });
+
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+const shape = Bodies.rectangle(200, 200, 50, 50, {
+    isStatic: true
+});
+// console.log(shape)
+World.add(world, shape)
+// console.log(world) => we can see all the shapes we created in the console 
