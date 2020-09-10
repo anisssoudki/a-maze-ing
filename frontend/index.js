cc824cc382aebfcf4bd780c996c569f8ac57d9e2
anisssoudki <aniss.marrouch@gmail.com>
2020-09-09 22:15:56 -0700

    designed sign-in sign-up functionality

--- frontend/index.js
+++ frontend/index.js
@@ -5,70 +5,95 @@
 
 
 document.addEventListener("DOMContentLoaded", event => {
+  renderForm()
 Player.fetchPlayer()
 // maze = new Maze(5,10)
 
 });
 
-const loginForm = document.createElement('form')
-loginForm.setAttribute("id","loginForm")
-
-const signUpForm = document.createElement('form')
-signUpForm.setAttribute("id","signUpForm")
-
-const signInDiv = document.createElement('div')
-signInDiv.setAttribute("id", "signInDiv")
-document.body.appendChild(signInDiv)
-signInDiv.appendChild(loginForm)
-signInDiv.appendChild(signUpForm)
-
-
-const signInButton = document.createElement('button')
-signInButton.setAttribute("id", "signInButton")
-signInButton.innerText = `sign_in`
-loginForm.appendChild(signInButton)
-
-
-const signUpButton = document.createElement('button')
-signUpButton.setAttribute("id", "signUpButton")
-signUpButton.innerText = `sign_up`
-signUpForm.appendChild(signUpButton)
-
-
-signInButton.addEventListener('click', event => {
-    let signInDiv = event.target.parentElement;
-    signInDiv.innerHTML = `<form id="loginForm"> 
-      <label> PlayerName </label>
-      <input name="name" type="text" placeholder="PlayerName">
-      <button id="signInBtn" type="submit">Submit</button>
-    </form>`;
-    
+function renderForm() {
+  const loginForm = document.createElement('form')
+  loginForm.setAttribute("id","loginForm")
+  
+  const signUpForm = document.createElement('form')
+  signUpForm.setAttribute("id","signUpForm")
+  
+  const signInDiv = document.createElement('div')
+  signInDiv.setAttribute("id", "signInDiv")
+  document.body.appendChild(signInDiv)
+  signInDiv.appendChild(loginForm)
+  signInDiv.appendChild(signUpForm)
+  
+  
+  const signInButton = document.createElement('button')
+  signInButton.setAttribute("id", "signInButton")
+  signInButton.innerText = `sign_in`
+  loginForm.appendChild(signInButton)
+  
+  
+  const signUpButton = document.createElement('button')
+  signUpButton.setAttribute("id", "signUpButton")
+  signUpButton.innerText = `sign_up`
+  signUpForm.appendChild(signUpButton)
+  
+  
+  signInButton.addEventListener('click', event => {
+      let signInDiv = event.target.parentElement;
+      signInDiv.innerHTML = `<form id="loginForm"> 
+        <label> PlayerName </label>
+        <input name="name" type="text" placeholder="PlayerName">
+        <button id="signInBtn" type="submit">Submit</button>
+      </form>`;
+      
+    });
+  
+    signUpButton.addEventListener('click', event => {
+        event.preventDefault()
+      let signInDiv = event.target.parentElement;
+      signInDiv.innerHTML = `<form id="signUpForm"> 
+        <label> NewPlayerName </label>
+        <input name="name" type="text" placeholder="NewPlayerName">
+        <button id="signUpBtn" type="submit">Submit</button>
+      </form>`;
+  
+  
   });
-
-  signUpButton.addEventListener('click', event => {
+       
+  signInDiv.addEventListener('submit', event => {
       event.preventDefault()
-    let signInDiv = event.target.parentElement;
-    signInDiv.innerHTML = `<form id="signUpForm"> 
-      <label> NewPlayerName </label>
-      <input name="name" type="text" placeholder="NewPlayerName">
-      <button id="signUpBtn" type="submit">Submit</button>
-    </form>`;
+      let input = event.target.name.value
+      let eventId = event.target.id 
+      // console.log(eventId)
+      // console.log(input)
+  
+      if (eventId === `signUpForm`) {
+      //  we are gonna create the player 
+      Player.createPlayer(input)
+      // chain .then here
+      .then(player => {
+        signInDiv.innerHTML = `<div class="signOutBanner">signed in as ${player.name}<button id="signout">signout</button></div>
+        <div id="userDiv" data-id="${player.id}"></div>`
+      })
+      }
+      else {
+            // we are gonna find the player 
+            Player.getPlayers(input)
+            .then(players =>
+               {         
+                  for (player of players) 
+                    {
+                          if (player.name.toLowerCase() === input.toLowerCase())
+                        signInDiv.innerHTML = `<div class="signOutBanner">welcome back ${player.name}<button id="signout">signout</button></div>
+                        <div id="userDiv" data-id="${player.id}"></div>`
+                  
+                    }
+                }
+              )
+           }
+  })
+  
 
-
-});
-     
-signInDiv.addEventListener('submit', event => {
-    event.preventDefault()
-    let input = event.target.name.value
-    let eventId = event.target.id 
-    // console.log(eventId)
-    // console.log(input)
-
-    if (eventId === `signUpForm`) {
-    //  we are gonna create the player 
-    Player.createPlayer(input)
-    }
-})
+}
 
     
  
--- frontend/modules/player.js
+++ frontend/modules/player.js
@@ -62,32 +62,53 @@ class Player
                 }
                 static renderPlayers() 
                 {
-                     let div = document.createElement("div")
-                     document.body.appendChild(div)
+                  
+                    
+                    let div = document.createElement("div")
+                   document.body.appendChild(div)
+
+                   
                      Player.all.forEach(function(element)
                                             {
+                                               
                                                 let h2 = document.createElement("h2") 
-                                                h2.innerText = `Player:${element.name}`
+                                                h2.textContent  = `Player:${element.name}`
                                                 div.appendChild(h2) 
+                                                
                                             }
                                         )
                 }
 
-                static playerObjCreation(event)
-                {
-                    event.preventDefault() 
-                    const form = event.target.elements 
-                    const playerName = form.playerName.value
-                    const PlayerObj =  Player.findPlayer(playerName)
-                    const configBodyObj = {}
-                    configBodyObj.player = PlayerObj
-                    createPlayer(configBodyObj)
-                }
+                // static playerObjCreation(event)
+                // {
+                //     event.preventDefault() 
+                //     const form = event.target.elements 
+                //     const playerName = form.playerName.value
+                //     const PlayerObj =  Player.findPlayer(playerName)
+                //     const configBodyObj = {}
+                //     configBodyObj.player = PlayerObj
+                //     createPlayer(configBodyObj)
+                // }
 
-            
+                static findPlayer(playerName) 
+                  {
+                    return (this.all.find(obj => {return obj.title === playerName}))
+                  }
+
+
+                  static getPlayers() {
+                    return fetch(this.baseUrl() + `players`).then(res => res.json());
+                  }
+
+
+                  static getPlayer(playerId) 
+                  {
+                    return fetch(this.baseUrl() + `users/${playerId}`).then(res => res.json());
+                  }
+
                 static createPlayer(name) 
                 {
                     return fetch(this.baseUrl() + `players`, this.fetchConfig("POST", {name}))
-                    .then(res => res.json().then(Player.fetchPlayer()));
+                    .then(res => res.json().then(this.renderPlayers()));
                 }
     }
