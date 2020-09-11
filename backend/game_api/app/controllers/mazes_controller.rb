b5ef163fceae16ae44b61405d102784e523d70a5
anisssoudki <aniss.marrouch@gmail.com>
2020-09-11 12:51:23 -0700

    built maze create in rails

--- backend/game_api/app/controllers/mazes_controller.rb
+++ backend/game_api/app/controllers/mazes_controller.rb
@@ -10,4 +10,20 @@ class MazesController < ApplicationController
         @maze = Maze.find(params[:id])
         render json: @maze
     end
+
+    def create
+        @maze = Maze.create(maze_params)
+        if @maze.valid?
+            render json: @maze
+        else
+            render json: { errors: @maze.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+
+    private 
+
+    def maze_params
+        params.require(:maze).permit(:difficulty,:time,:player_id)
+    end
 end
